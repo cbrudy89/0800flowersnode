c47d87412fac19ead23a96448cce866a6ec91724
Vikram Singh <vikram@mobikasa.com>
2017-07-31 21:15:58 +0530

    Resolved Conflict

--- api/validation/product-validation.js
+++ api/validation/product-validation.js
@@ -7,14 +7,9 @@ module.exports = {
 	    slug: Joi.string().required(),
 	    language_id: Joi.string().required(),
 	    country_id: Joi.string().required(),
-<<<<<<< HEAD
 	    postalcode: Joi.string().required(),
 	    currency_id: Joi.string().required(),
 	    recent_products: Joi.string()
-=======
-	    currency_id: Joi.string().required(),
-	    postalcode: Joi.string().required()
->>>>>>> 868455bfd2caa0f9f8391ef309f554c12541522f
 	  }  	
   }
 };
