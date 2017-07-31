3d15ee2dc0bfdfec75b0297c8e527f8777a8a281
Vikram Singh <vikram@mobikasa.com>
2017-07-31 21:13:06 +0530

    PDP Workd

--- api/helpers/common-helper.js
+++ api/helpers/common-helper.js
@@ -71,6 +71,108 @@ function CommonHelper(){
 
 	}
 
+	this.getCurrencyDetails = function($currency_id = null, $country_id, callback){
+
+	  // Get price details from currency tables by country
+	  var sql = "SELECT c.* FROM country_list cl LEFT JOIN currency c ON(cl.preferred_currency_id = c.id) WHERE cl.id = "+$country_id+" AND c.status = 1";
+
+	  //console.log(sql);
+
+	  dbModel.rawQuery(sql, function(err, currency_result) {
+	    if (err) return callback(err);
+	    else if (currency_result.length > 0){
+	       callback(null, currency_result);
+	                      
+	    }else{
+
+	      // If Preferred Currency Id not found for selected country then Use selected Currency Rate
+	      var $sql = "SELECT * from `currency` WHERE id = "+$currency_id;
+
+	      dbModel.rawQuery($sql, function(err, currency_result) {
+	        if (err) return callback(err);
+	        else 
+	            callback(null, currency_result);
+
+	      });
+	    }
+	  });
+
+	}
+
+	this.number_format = function(number, decimals, decPoint, thousandsSep, callback) { // eslint-disable-line camelcase
+		  number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+		  var n = !isFinite(+number) ? 0 : +number;
+		  var prec = !isFinite(+decimals) ? 0 : Math.abs(decimals);
+		  var sep = (typeof thousandsSep === 'undefined') ? ',' : thousandsSep;
+		  var dec = (typeof decPoint === 'undefined') ? '.' : decPoint;
+		  var s = '';
+		  var toFixedFix = function (n, prec) {
+		    var k = Math.pow(10, prec);
+		    return '' + (Math.round(n * k) / k).toFixed(prec);
+		  }
+		  // @todo: for IE parseFloat(0.55).toFixed(0) = 0;
+		  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.')
+		  if (s[0].length > 3) {
+		    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+		  }
+		  if ((s[1] || '').length < prec) {
+		    s[1] = s[1] || '';
+		    s[1] += new Array(prec - s[1].length + 1).join('0');
+		  }
+		  //console.log(s.join(dec));
+		  callback(null, s.join(dec));
+	}
+
+	this.roundToNineNine = function($actPrice, $current_currency, callback){ 
+	    $newPrice = false;
+	    if($actPrice != '' && $actPrice != undefined){
+	        $parse = $actPrice.split(".");
+
+	        if($current_currency === "INR"){
+	            $price = $parse[0];
+	            $priceLength = $price.length;
+	                    
+	            switch ($priceLength) {
+	                case 1: 
+	                    $newPrice = $price.substring(0, 1) + '9';
+	                    
+	                    break;
+	                case 2: 
+	                    $newPrice = $price.substring(0, 2) + '99';
+	                    
+	                    break;                    
+	                case 3: 
+	                    $newPrice = $price.substring(0, 1) + '99'; // Below 999
+	                    
+	                    break;
+	                case 4: 
+	                    $newPrice = $price.substring(0, 2) + '99'; // For 1000 or < 9999
+	                    
+	                    break;                    
+
+	                case 5: 
+	                      $newPrice = $price.substring(0, 3) + '99'; // For 1,000/10000 or < 9,999
+	                    
+	                    break;
+	                case 6: 
+	                    $newPrice = $price.substring(0, 4) + '99'; // For 10,000/100000
+	                    
+	                    break;
+	                case 7: 
+	                    $newPrice = $price.substring(0, 5) + '99'; // For 1000000
+	                    break;
+
+	                case 8: 
+	                    $newPrice = $price.substring(0, 6) + '99'; // For 1,00,000/10000000
+	                    break;
+	            }
+	        }else{ 
+	            $newPrice = $parse[0]+".99";
+	        }
+	    }
+	    callback(null, $newPrice);
+	}
+
 	/*this.checkCalSurcharge = function($date, $vendor_id, $country_id, $product_id){
 
 	    var sql = "SELECT description FROM `snipes` inner join `snipe_language` on `snipes`.`id` = `snipe_language`.`snipe_id` WHERE (`snipes`.`type` = '"+type+"' and `snipes`.`status` = 1 and `snipe_language`.`language_id` = "+language_id+") order by RAND() limit 1";
--- api/validation/product-validation.js
+++ api/validation/product-validation.js
@@ -7,7 +7,9 @@ module.exports = {
 	    slug: Joi.string().required(),
 	    language_id: Joi.string().required(),
 	    country_id: Joi.string().required(),
-	    postalcode: Joi.string().required()
+	    postalcode: Joi.string().required(),
+	    currency_id: Joi.string().required(),
+	    recent_products: Joi.string()
 	  }  	
   }
 };
