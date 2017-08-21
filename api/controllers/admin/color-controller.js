f8799763aa34a9e6df045e317a4f81e0646a7baf
Vikram Singh <vikram@mobikasa.com>
2017-08-21 19:16:46 +0530

    Worked on color and mixed bouquet API

--- api/controllers/admin/color-controller.js
+++ api/controllers/admin/color-controller.js
@@ -1,7 +1,9 @@
 var jwt=require('jsonwebtoken');
+var Sync = require('sync');
 var bcrypt = require('bcrypt');
 var config = require('./../../../config');
 var connection = require('./../../../database');
+var dbModel = require('./../../models/db-model');
 //var userHelper = require('./../helpers/user-helper');
 var colorModel = require('./../../models/admin/color-model');
 
@@ -16,19 +18,39 @@ function ColorController() {
         message: "You dont have permission to get color!"
       });       
     }else{
-      var color_name = req.body.color_name;
-      colorModel.getcolors(color_name, function(err, result){
+
+      var color_name = req.body.color_name ? req.body.color_name : "" ;
+
+      colorModel.getcolors(color_name, function(err, results){
          if(err) {
-            console.log(err);
+        
+          res.status(config.HTTP_SERVER_ERROR).send({
+            status: config.ERROR, 
+            code : config.HTTP_SERVER_ERROR, 
+            message : "Unable to get colors!",
+            errors : err
+          });
+        
          }else{
+
+          Sync(function(){
+            for(var i=0; i < results.length; i++) {
+              var languagedata = colorModel.getLanguageData.sync(null,results[i].id);
+              results[i].color = languagedata;
+            }
+            
             res.status(config.HTTP_SUCCESS).send({
               status: config.SUCCESS, 
               code : config.HTTP_SUCCESS, 
-              message: 'all colors found',
-              data : result
+              message: 'All colors found',
+              result : results
             });
+
+          });
+
          }
       });
+
     } // else close    
 
   }
@@ -39,43 +61,102 @@ function ColorController() {
       res.status(config.HTTP_FORBIDDEN).send({
         status: config.ERROR, 
         code : config.HTTP_FORBIDDEN, 
-        message: "You dont have permission to create user!"
+        message: "You dont have permission to create color!"
       });       
     }else{
-      
+
       // Insert into color table
       var curr_date  = new Date();
-      var id=0;
-      colorData = {
-            'color_name':req.body.color_name,
-            'status':'1',
-            'created_at':curr_date,
-            'updated_at':curr_date
-      };
-      colorModel.checkcolor(colorData,id, function(err, result){
-        //console.log(err);
-         if(result.length > 0 && result[0].id > 0){
-            res.status(config.HTTP_ALREADY_EXISTS).send({
-              status: config.ERROR, 
-              code : config.HTTP_ALREADY_EXISTS, 
-              message: "The specified color already exists."
-            });
-         }else{
-            colorModel.createcolor(colorData, function(err, result){
-              //console.log(err);
-               if(err) {
-                  console.log(err);
-               }else{
+
+      var colorArray= req.body.colorArray;
+      var color_code = req.body.color_code;
+      var status = req.body.status;
+
+      Sync(function(){
+
+          var found = 0;
+          var jsonData = JSON.parse(colorArray);
+          //var jsonData = colorArray;
+          for (var i = 0; i < jsonData.length; i++) {
+            var lang = jsonData[i];
+            var isExist = isColorExist.sync(null, '', lang);
+            //console.log(isExist);
+            if(isExist == 'true') found++;
+          }
+
+          // Check Color already exist.
+          if(found > 0){
+
+              res.status(config.HTTP_ALREADY_EXISTS).send({
+                status: config.ERROR, 
+                code : config.HTTP_ALREADY_EXISTS, 
+                message: "The specified color already exists."
+              });
+
+          }else{
+
+            //console.log('asdf');
+            var colorData = {
+              "color_code": color_code,
+              'status': status,
+              'created_at':curr_date,
+              'updated_at':curr_date,
+              "color_name": "test"
+            };
+
+            dbModel.save('colors', colorData, "", function(err, result) {
+              if (err) {                
+                  res.status(config.HTTP_SERVER_ERROR).send({
+                    status: config.ERROR, 
+                    code : config.HTTP_SERVER_ERROR, 
+                    message : "Unable to create color!",
+                  }); 
+
+              }else{
+
+                if(result.insertId > 0){
+                  var type_id = result.insertId;
+
+                  if(colorArray.length > 0){
+
+                     Sync(function(){
+                    
+                      var jsonData = JSON.parse(colorArray);
+                      //var jsonData = colorArray;
+                      for (var i = 0; i < jsonData.length; i++) {
+                        var color = jsonData[i];
+                        var data = {
+                          "type": "color",
+                          "type_id": type_id,
+                          "language_id": color.language_id,
+                          "name": color.name,                          
+                          "description": "",
+                        };
+
+                        colorModel.createcolor.sync(null, data);
+                      } 
+
+                    });
+                    
+                  }
+
                   res.status(config.HTTP_SUCCESS).send({
                     status: config.SUCCESS, 
                     code : config.HTTP_SUCCESS, 
-                    message: 'new colors has been created',
+                    message: 'New color has been created'
                   });
-               }
+                
+                }else{
+                  res.status(config.HTTP_SERVER_ERROR).send({
+                    status: config.ERROR, 
+                    code : config.HTTP_SERVER_ERROR, 
+                    message : "Unable to create color!",
+                  });
+                }
+              }              
             });
-         }
-      }); 
-
+          }
+      });
     }    
   }
 
@@ -90,42 +171,101 @@ function ColorController() {
         message: "You dont have permission to update color!"
       });       
     }else{
+
       var curr_date  = new Date();
-      var id =req.body.id;
-      colorData = {
-            'color_name':req.body.color_name,
-            'status':req.body.status,
-            'updated_at':curr_date
-      };
-      colorModel.checkcolor(colorData,id, function(err, result){
-        //console.log(err);
-         if(result.length > 0 && result[0].id > 0){
-            res.status(config.HTTP_ALREADY_EXISTS).send({
-              status: config.ERROR, 
-              code : config.HTTP_ALREADY_EXISTS, 
-              message: "the specified color name already exists."
-            });
-         }else{
-            colorModel.updatecolor(colorData,id, function(err, result){
-              //console.log(err);
-               if(err) {
-                  console.log(err);
-               }else{
+      var id= req.body.id;
+
+      var colorArray = req.body.colorArray;
+      var color_code = req.body.color_code;
+      var status = req.body.status;
+
+      Sync(function(){
+
+          var found = 0;
+          var jsonData = JSON.parse(colorArray);
+          //var jsonData = colorArray;
+          for (var i = 0; i < jsonData.length; i++) {
+            var lang = jsonData[i];
+            var isExist = isColorExist.sync(null, id, lang);
+            //console.log(isExist);
+            if(isExist == 'true') found++;
+          }
+
+          // Check color already exist.
+          if(found > 0){
+
+              res.status(config.HTTP_ALREADY_EXISTS).send({
+                status: config.ERROR, 
+                code : config.HTTP_ALREADY_EXISTS, 
+                message: "The specified color already exists."
+              });
+
+          }else{
+
+            //console.log('asdf');
+            var colorData = {
+              'color_code': color_code,
+              'status': status,
+              'updated_at':curr_date
+            };
+
+            dbModel.save('colors', colorData, id, function(err, result) {
+              if (err) {                
+                  res.status(config.HTTP_SERVER_ERROR).send({
+                    status: config.ERROR, 
+                    code : config.HTTP_SERVER_ERROR, 
+                    message : "Unable to update color!",
+                  }); 
+
+              }else{
+
+                //console.log(result);
+
+                if(result.affectedRows > 0){
+
+                  if(colorArray.length > 0){
+
+                     Sync(function(){
+                    
+                      var jsonData = JSON.parse(colorArray);
+                      //var jsonData = colorArray;
+                      for (var i = 0; i < jsonData.length; i++) {
+                        var color = jsonData[i];
+                        var data = {
+                          "type": "color",
+                          "name": color.name,                          
+                          "description": "",
+                        };
+
+                        colorModel.updatecolor.sync(null, data, id, color.language_id);
+                      } 
+
+                    });
+                    
+                  }
+
                   res.status(config.HTTP_SUCCESS).send({
                     status: config.SUCCESS, 
                     code : config.HTTP_SUCCESS, 
-                    message: 'the colors has been updated',
+                    message: 'The color has been updated'
                   });
-               }
-            });
-         }
-      });
+                
+                }else{
+                  res.status(config.HTTP_SERVER_ERROR).send({
+                    status: config.ERROR, 
+                    code : config.HTTP_SERVER_ERROR, 
+                    message : "Unable to update color!",
+                  }); 
 
-      
+                }
+
+              }              
+            });
+          }
+      });
     }  
   }
 
-  // delete color 
   this.deletecolor = function(req,res){
     if(req.decoded.role != config.ROLE_ADMIN){
       res.status(config.HTTP_FORBIDDEN).send({
@@ -138,55 +278,116 @@ function ColorController() {
       var id = req.body.id;
       //console.log("id-"+id);
       colorModel.checkdeletecolor(id, function(err, result){
-       // console.log(result);
-         if(!result.length){
-            res.status(config.HTTP_NOT_FOUND).send({
-              status: config.ERROR, 
-              code : config.HTTP_NOT_FOUND, 
-              message: "The specified color not found."
-            });
-         }else{
-            colorModel.deletecolor(id, function(err, result){
-              //console.log(err);
-               if(err) {
-                  console.log('test'+err);
-               }else{
-                  res.status(config.HTTP_SUCCESS).send({
-                    status: config.SUCCESS, 
-                    code : config.HTTP_SUCCESS, 
-                    message: 'the colors has been deleted',
-                  });
-               }
-            });
-         }
+
+        if(err){
+          //console.log(err);
+          res.status(config.HTTP_SERVER_ERROR).send({
+            status: config.ERROR, 
+            code : config.HTTP_SERVER_ERROR, 
+            message : "Unable to delete color!",
+            errors : err
+          });
+
+        }else {
+
+           if(!result.length){
+              res.status(config.HTTP_NOT_FOUND).send({
+                status: config.ERROR, 
+                code : config.HTTP_NOT_FOUND, 
+                message: "The specified color not found."
+              });
+           }else{
+              colorModel.deletecolor(id, function(err, result){
+                //console.log(err);
+                 if(err) {
+                    res.status(config.HTTP_SERVER_ERROR).send({
+                      status: config.ERROR, 
+                      code : config.HTTP_SERVER_ERROR, 
+                      message : "Unable to delete color!",
+                      errors : err
+                    });
+                 }else{
+                    res.status(config.HTTP_SUCCESS).send({
+                      status: config.SUCCESS, 
+                      code : config.HTTP_SUCCESS, 
+                      message: 'The color has been deleted',
+                    });
+                 }
+              });
+           }
+        }
       });
-
-    } // else close    
-
+    }
   }
 
   // Get color Information 
   this.getcolor = function(req, res) {
+    
     var id=req.body.id;
     //console.log(id);
-    connection.acquire(function(err, con) {
+ 
+    colorModel.getcolor(id, function(err, result) {
       if (err) {
-        res.send({status: 1, message: err});
-      }      
-      con.query('select * from colors where id = ?', [id], function(err, result) {
-        if (err) {
-          res.send({status: 1, message: 'Failed to get'});
-        } else {
-          if(result.length > 0){
-            res.send({status: 0, message: 'color found!', response: result});
-          }else{
-            res.send({status: 1, message: 'Failed to get color'});
-          }
-        }        
-        con.release();
-      });
+        res.status(config.HTTP_SERVER_ERROR).send({
+          status: config.ERROR, 
+          code : config.HTTP_SERVER_ERROR, 
+          message : "Unable to get color!",
+          errors : err
+        });
+      } else {
+        if(result.length > 0){
+
+            Sync(function(){
+
+              var languagedata = colorModel.getLanguageData.sync(null,result[0].id);
+              result[0].color = languagedata;
+
+              res.status(config.HTTP_SUCCESS).send({
+                status: config.SUCCESS, 
+                code : config.HTTP_SUCCESS, 
+                message: 'Color found!',
+                result: result
+              });
+
+            });
+
+
+        }else{
+          res.status(config.HTTP_NOT_FOUND).send({
+            status: config.ERROR, 
+            code : config.HTTP_NOT_FOUND, 
+            message : "Color not found.",
+            errors : err
+          });
+        }
+      }        
     });
-  };
+    
+  }
+
+}
+
+function isColorExist(id = '', data, callback){
+
+  var sql = "SELECT id FROM language_types WHERE type='color' AND language_id ="+data.language_id+" AND name = '"+data.name+"' AND name != ''";
+  //console.log(sql);
+  if(id != '' && id > 0){
+    sql += " AND type_id <> "+id;
+  }
+
+  sql += "  LIMIT 1";
+  
+  dbModel.rawQuery(sql, function(err, result){
+    if (err) {
+      callback(err);
+    }else{
+      if(result.length > 0 && result[0].id > 0){
+        callback(null, 'true');
+      }else{
+        callback(null, 'false');
+      }
+    }
+  });
 
 }
 
--- api/models/admin/color-model.js
+++ api/models/admin/color-model.js
@@ -1,127 +1,143 @@
 var config = require('./../../../config');
 var connection = require('./../../../database');
+var dbModel = require('./../../models/db-model');
+var table_name = "colors";
+var type = "color";
 
 function colorModel(){	
 	this.getcolors = function(color_name, callback) {
-		connection.acquire(function(err, con) {
+
+	    var sql = "SELECT c.id, c.color_code, c.created_at, c.updated_at, CASE WHEN c.status = 1 THEN 'Active' WHEN c.status = 0 THEN 'In-Active' END AS 'status' FROM "+table_name+" c INNER JOIN language_types lt ON(c.id = lt.type_id) WHERE lt.type='"+type+"'";
+	    if(color_name != "" && color_name != undefined){
+	        sql += " AND lt.name like '%"+color_name+"%'";
+	    }
+
+	    sql += " GROUP BY c.id";
+
+	    //console.log(sql);
+
+		dbModel.rawQuery(sql, function (err, results) {
+	      	if (err) {
+	      		callback(err);
+	      	}else{
+	      		callback(null, results);
+	      	}
+	  	});
+	}
+
+
+	this.getcolor = function(id, callback) {
+
+    	dbModel.rawQuery("SELECT id,color_code,status,created_at,updated_at FROM "+table_name+" WHERE id = "+id, function(err, result){            	
+          if (err) {
+            callback(err);
+          }else{
+          	callback(null,result);
+          }
+        });			
+	}
+
+	this.createcolor = function(data, callback) {
+		dbModel.save("language_types", data, "", function (err, results) {
+	      	if (err) {
+	      		callback(err);
+	      	}else{
+	      		callback(null, results);
+	      	}			          	
+	  	});
+	}
+
+
+	this.updatecolor = function(data, type_id, language_id, callback) {
+		dbModel.getConnection(function(err, con){
 			if (err) {
 				callback(err);
 			}
-			else {	
-			    var queryString = "SELECT * FROM colors";
-			    if(color_name != "" && color_name != undefined){
-			        queryString += " WHERE `color_name` like '%"+color_name+"%'";
-			    }
-				con.query(queryString, function (err, results) {
+			else {
+				con.query('UPDATE language_types SET ? WHERE type_id = ? AND language_id = ?', [data, type_id, language_id], function (err, results) {
+					con.release();
 		          	if (err) {
 		          		callback(err);
 		          	}else{
 		          		callback(null, results);
-		          	}
-		          	con.release();
+		          	}			          	
 		      	});
 			}
-		});
-	}
-
-	this.checkcolor = function(data,id, callback) {
-		connection.acquire(function(err, con) {
-			if (err) {
-				callback(err);
-			}
-			else {	
-				// Checking if user email already exist in database
-            	con.query('SELECT id FROM colors WHERE color_name = ? AND id <> ?', [data.name,id], function(err, result){            	
-	              if (err) {
-	                callback(err);
-	              }else{
-	              	callback(null,result);
-	              }
-	              con.release();
-	            });
-			    
-			}
-		});
-	}
-	this.createcolor = function(data, callback) {
-		connection.acquire(function(err, con) {
-			if (err) {
-				callback(err);
-			}
-			else {	
-				// Checking if user email already exist in database
-	        		con.query('INSERT INTO colors SET ?', data, function (err, results) {
-			          	if (err) {
-			          		callback(err);
-			          	}else{
-			          		callback(null, results);
-			          	}			          	
-			      	});
-	//////////////////////////////////////////////////////
-			    
-			}
-		});
-	}
-
-
-	this.updatecolor = function(data,id, callback) {
-		connection.acquire(function(err, con) {
-			if (err) {
-				callback(err);
-			}
-			else {	
-				// Checking if user email already exist in database
-	        		con.query("Update `colors` set `color_name` = '"+data.color_name+"', `status` = '"+data.status+"', `updated_at` = '"+data.updated_at+"' where id='"+id+"'", function (err, results) {
-			          	if (err) {
-			          		callback(err);
-			          	}else{
-			          		callback(null, results);
-			          	}			          	
-			      	});
-	//////////////////////////////////////////////////////
-			    
-			}
-		});
+		});	
 	}
 
 	this.checkdeletecolor = function(id, callback) {
-		connection.acquire(function(err, con) {
-			if (err) {
-				callback(err);
-			}
-			else {	
-				// Checking if user email already exist in database
-            	con.query('SELECT id FROM colors WHERE id = ?', [id], function(err, result){            	
-	              if (err) {
-	                callback(err);
-	              }else{
-	              	callback(null,result);
-	              }
-	              con.release();
-	            });
-			    
-			}
-		});
+
+		// Checking if user email already exist in database
+    	dbModel.rawQuery("SELECT id FROM "+table_name+" WHERE id = "+id, function(err, result){            	
+          if (err) {
+            callback(err);
+          }else{
+          	callback(null,result);
+          }
+        });
+
 	}
 
 	this.deletecolor = function(id, callback) {
-		connection.acquire(function(err, con) {
-			if (err) {
-				callback(err);
-			}
-			else {	
-				// Checking if user email already exist in database
-	        		con.query("Delete from `colors` where id=?",[id], function (err, results) {
-			          	if (err) {
-			          		callback(err);
-			          	}else{
-			          		callback(null, results);
-			          	}			          	
-			      	});
-	//////////////////////////////////////////////////////
-			    
-			}
-		});
+
+		// Delete color types
+		dbModel.getConnection(function(error, con){
+          if (error) {
+            callback(error);
+          }else{
+
+            // Delete colors table if found
+            dbModel.beginTransaction(con, 'DELETE FROM '+table_name+' WHERE id ='+id, function(error, result){
+              if(error){
+                callback(error);                   
+              }else{
+
+                if(result.affectedRows > 0){
+
+                  // Delete vendor specific entries form country vendor,group_vendor,method_vendor and vendor_secondary_contact tables.
+                  var sql = "DELETE FROM language_types WHERE type='"+type+"' AND type_id="+id;
+                  //console.log(sql);
+
+                  // Delete vendor specific entries form group_vendor table
+                  dbModel.transactionQuery(con, sql, function (error, result) {
+                    if (error) {
+                      callback(error);
+                    }else{
+
+                      dbModel.commit(con, function(err, response){
+                        if (error) {
+                          callback(error);
+                        }else{
+                          callback(null, response);                                   
+                        }                                  
+
+                      });
+
+                    }    
+                  });
+               
+                }else{
+                  callback(error);
+                }
+
+              }
+
+            });
+
+          }
+
+        });
+	}	
+
+	this.getLanguageData = function(type_id, callback){
+
+	    $sql = "SELECT language_id,name from `language_types` WHERE type='"+type+"' AND type_id="+type_id;
+	    dbModel.rawQuery($sql, function(err, $result) {
+	        if (err) callback(err);
+	        else callback(null,$result);
+	    });
+
 	}	
 }
 
